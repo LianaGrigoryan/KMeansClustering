ca5a2aeeffd7d4fab5bc719f56e457ee0414aa3e
Mike Yeung <mikeyeung123@gmail.com>
2015-03-07 20:01:16 -0500

    Added blank files

---
+++ KMeansClustering/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KMeansClustering
+//
+//  Created by Mike Yeung on 3/7/15.
+//  Copyright (c) 2015 Mike Yeung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
